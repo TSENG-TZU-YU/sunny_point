e827ba5863598c814903ab572fea7902b8282f7c
TSENG-TZU-YU <46403343+TSENG-TZU-YU@users.noreply.github.com>
2023-02-13 17:18:08 +0800

    header exchang sreach

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,21 @@ import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 //子頁面
 import Header from './Header';
+import Exchang from './Exchang';
+import Search from './Search';
 
 function App() {
+
+
+    return <Header />
+    return <Exchang />
+    return <Search />
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/Header" element={<Header />} />
+                <Route path="/header" element={<Header />} />
+                <Route path="/exchange" element={<Exchang />} />
+                <Route path="/search" element={<Search />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ src/Exchang/index.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import './app.scss';
+
+import top1 from '../assets/two/點數活動頁5-08.png';
+import top2 from '../assets/two/點數活動頁5-09.png';
+import one from '../assets/two/點數活動頁5-10.png';
+import two from '../assets/two/點數活動頁5-11.png';
+import three from '../assets/two/點數活動頁5-12.png';
+import four from '../assets/two/點數活動頁5-13.png';
+
+import { Link, animateScroll } from 'react-scroll';
+
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
+function Exchang() {
+    return (
+        <div className="exContainer">
+            <div className="title">
+                <div className="h1">點數兌換點數</div>
+                <div className="line"></div>
+            </div>
+            <div className="nav">
+                <div className="index ">
+                    <LazyLoadImage src={top1} alt="" />
+                </div>
+                <div className="index ">
+                    <LazyLoadImage src={top2} alt="" />
+                </div>
+            </div>
+            <div className="exContain">
+                <div className="imageContainer1 ">
+                    <LazyLoadImage src={one} alt="" />
+                </div>
+                <div className="imageContainer1 mt">
+                    <LazyLoadImage src={two} alt="" />
+                </div>
+                <div className="imageContainer1 mt">
+                    <LazyLoadImage src={three} alt="" />
+                </div>
+                <div className="imageContainer1 mt">
+                    <LazyLoadImage src={four} alt="" />
+                    <a href="https://www.sunnygo.com.tw/web-front/pointExchange" alt="/">
+                        {' '}
+                    </a>
+                    <a href="https://www.sunnygo.com.tw/web-front/pointExchange" alt="/">
+                        {' '}
+                    </a>
+                    <a href="https://www.sunnygo.com.tw/web-front/pointExchange" alt="/">
+                        {' '}
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Exchang;
