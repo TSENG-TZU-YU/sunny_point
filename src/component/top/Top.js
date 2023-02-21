377734f9c0177b79d9551c39cefd121b4afe1b3e
Sandy <a0919613087@gmail.com>
2023-02-21 15:05:33 +0800

    update TOP

--- src/component/top/Top.js
+++ src/component/top/Top.js
@@ -3,9 +3,9 @@ import './_top.scss';
 
 function Top() {
     return (
-        <div className="toTop">
-            <a href="#top">TOP</a>
-        </div>
+        <a className="toTop" href="#top">
+            <div>TOP</div>
+        </a>
     );
 }
 
